a7bc67014293cf297307b801d70875a74df2afe0
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-26 05:55:13 +0700

    Update supabase.js

--- api/supabase.js
+++ api/supabase.js
@@ -1,12 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
-
-console.log("SUPABASE_URL:", supabaseUrl);
-console.log("KEY ADA:", !!supabaseKey);
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
+export const supabase =
+createClient(
+process.env.SUPABASE_URL,
+process.env.SUPABASE_ANON_KEY
 );
