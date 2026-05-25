be333595e0e7166fe0c5713b657fae7dd75925fc
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 22:39:19 +0700

    Update deposit.js

--- api/deposit.js
+++ api/deposit.js
@@ -1,54 +1,14 @@
-import { supabase } from './supabase.js';
-
 export default async function handler(req,res){
 
 try{
 
-if(req.method!=="POST"){
-
-return res.status(405).json({
-status:false,
-message:'Method tidak diizinkan'
-});
-
-}
-
-const {
-nominal,
-metode,
-bukti
-}=req.body;
-
-if(!nominal){
-
-return res.json({
-status:false,
-message:'Nominal kosong'
-});
-
-}
-
-const {error}=await supabase
-.from('deposits')
-.insert([{
-nominal:Number(nominal),
-metode,
-bukti,
-status:'pending'
-}]);
-
-if(error){
-
-return res.status(500).json({
-status:false,
-message:error.message
-});
-
-}
-
-return res.json({
+return res.status(200).json({
 status:true,
-message:'Deposit menunggu ACC'
+node:"jalan",
+env:{
+url: !!process.env.SUPABASE_URL,
+key: !!process.env.SUPABASE_ANON_KEY
+}
 });
 
 }catch(err){
