069a033a654cebfd2948d4885f33325c4ddf6f58
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-26 05:54:46 +0700

    Update deposits.js

--- api/deposits.js
+++ api/deposits.js
@@ -1,9 +1,36 @@
+import { supabase } from "./supabase.js";
+
 export default async function handler(req,res){
+
+try{
+
+const { data, error } =
+await supabase
+.from("deposits")
+.select("*")
+.eq("status","pending");
+
+if(error){
+
+return res.status(500).json({
+status:false,
+message:error.message
+});
+
+}
 
 return res.status(200).json({
 status:true,
-url: process.env.SUPABASE_URL,
-keyExists: !!process.env.SUPABASE_ANON_KEY
+data:data || []
 });
+
+}catch(err){
+
+return res.status(500).json({
+status:false,
+message:err.message
+});
+
+}
 
 }
