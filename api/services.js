ebbc51c751243e4461d23dbeaabf9703935ffc61
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 15:12:16 +0700

    Create services.js

---
+++ api/services.js
@@ -0,0 +1,105 @@
+export default async function handler(req, res) {
+
+    try {
+
+        const api_id =
+        process.env.API_ID;
+
+        const api_key =
+        process.env.API_KEY;
+
+        const formData =
+        new URLSearchParams();
+
+        formData.append(
+            'api_id',
+            api_id
+        );
+
+        formData.append(
+            'api_key',
+            api_key
+        );
+
+        /*
+        =========================
+        REQUEST SERVICES
+        =========================
+        */
+
+        const response = await fetch(
+
+            'https://fayupedia.id/api/services',
+
+            {
+
+                method:'POST',
+
+                headers:{
+                    'Content-Type':
+                    'application/x-www-form-urlencoded'
+                },
+
+                body:formData.toString()
+
+            }
+
+        );
+
+        const data =
+        await response.json();
+
+        /*
+        =========================
+        TAMBAH PROFIT 20%
+        =========================
+        */
+
+        const services =
+        data.map(service => {
+
+            const originalPrice =
+            Number(service.price);
+
+            const sellPrice =
+            Math.ceil(
+                originalPrice * 1.2
+            );
+
+            return {
+
+                id: service.id,
+
+                name: service.name,
+
+                min: service.min,
+
+                max: service.max,
+
+                price: sellPrice
+
+            };
+
+        });
+
+        return res.status(200).json({
+
+            status:true,
+
+            services:services
+
+        });
+
+    } catch(err) {
+
+        return res.status(500).json({
+
+            status:false,
+
+            message:'Server Error'
+
+        });
+
+    }
+
+}
