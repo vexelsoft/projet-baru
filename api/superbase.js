1af51f0b6bd89b58619dd65e2e54fe21206e5218
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 22:42:59 +0700

    Update superbase.js

--- api/superbase.js
+++ api/superbase.js
@@ -1,16 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error(
-    'SUPABASE_URL atau SUPABASE_ANON_KEY belum terisi'
-  );
-}
-
-export const supabase =
-createClient(
+export const supabase = createClient(
   supabaseUrl,
   supabaseKey
 );
