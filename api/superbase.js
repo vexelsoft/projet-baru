26566f7930464755e20f4b4e329b7a35ea54cf5e
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 23:22:20 +0700

    Update superbase.js

--- api/superbase.js
+++ api/superbase.js
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
+export const supabase =
+createClient(
+process.env.SUPABASE_URL,
+process.env.SUPABASE_ANON_KEY
 );
