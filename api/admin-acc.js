6c26beb3757c7e2f6e6ba9984b8173353b008842
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 22:10:42 +0700

    Update admin-acc.js

--- api/admin-acc.js
+++ api/admin-acc.js
@@ -1,28 +1,6 @@
-let deposits=[];
-
-export default function handler(req,res){
-
+import {supabase} from './supabase'
+export default async function handler(req,res){
 const {depositId}=req.body;
-
-const deposit=
-deposits.find(
-x=>x.id==depositId
-);
-
-if(!deposit){
-
-return res.json({
-status:false,
-message:'Deposit tidak ditemukan'
-});
-
-}
-
-deposit.status='success';
-
-res.json({
-status:true,
-message:'Deposit berhasil di ACC'
-});
-
+await supabase.from('deposits').update({status:'success'}).eq('id',depositId);
+res.json({status:true,message:'Deposit berhasil di ACC'})
 }
