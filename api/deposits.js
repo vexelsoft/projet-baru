47d7b05afa89d45be2a42ddc19c8b45dead87074
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 23:50:01 +0700

    Update deposits.js

--- api/deposits.js
+++ api/deposits.js
@@ -1,35 +1,8 @@
-import { supabase } from "./supabase.js";
-
-export default async function handler(req,res){
-
-try{
-
-const { data, error } =
-await supabase
-.from("deposits")
-.select("*");
-
-if(error){
-
-return res.status(500).json({
-status:false,
-message:error.message
-});
-
-}
+export default function handler(req,res){
 
 return res.status(200).json({
 status:true,
-data:data || []
+message:"deposits.js berjalan"
 });
-
-}catch(err){
-
-return res.status(500).json({
-status:false,
-message:err.message
-});
-
-}
 
 }
