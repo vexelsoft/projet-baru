35c04678d5cfee5b12c4405a9f7d57e9bf5f784c
arif <161019889+vexelsoft@users.noreply.github.com>
2026-05-25 21:25:56 +0700

    Update deposits.js

--- api/deposits.js
+++ api/deposits.js
@@ -1,32 +1,12 @@
-import fs from "fs";
-import path from "path";
+let deposits=[];
 
 export default function handler(req,res){
 
-const filePath=
-path.join(
-process.cwd(),
-"data",
-"deposits.json"
-);
-
-const deposits=
-JSON.parse(
-fs.readFileSync(
-filePath,
-"utf8"
-)
-);
-
 res.json({
-
 status:true,
-
-data:
-deposits.filter(
+data:deposits.filter(
 x=>x.status==="pending"
 )
-
 });
 
 }
